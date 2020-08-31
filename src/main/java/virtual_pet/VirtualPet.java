e7b28a6c2a18a0ece5b6edf34cf4fef71b48e149
robertson355 <barry.robertson1993@gmail.com>
2020-08-31 16:42:16 -0400

    setup methods for project

---
+++ src/main/java/virtual_pet/VirtualPet.java
@@ -0,0 +1,61 @@
+package virtual_pet;
+
+public class VirtualPet {
+
+   int  hunger = 40;
+   int thirst = 100;
+   int boredom = 40;
+   int tiredness = 120;
+
+   public void tick() {
+
+    hunger = hunger - 4;
+    thirst =  thirst - 2;
+
+
+   }
+
+    public void feed() {
+
+
+       hunger = hunger + 20;
+       thirst = thirst - 5;
+
+    }
+
+    public void drink() {
+       hunger = hunger - 5;
+       thirst = thirst + 15;
+    }
+
+    public void play() {
+       hunger = hunger -5;
+       thirst = thirst -10;
+       boredom = boredom + 30;
+    }
+
+    public void sleep() {
+       hunger = hunger -10;
+       thirst = thirst -10;
+       tiredness = tiredness + 30;
+    }
+
+
+
+    public boolean random() {
+        int random = (int) (1 + (Math.random() * 5));
+        System.out.println("Random = " + random);
+        if (random == 2) {
+            System.out.println("Your pet decided to eat!");
+            hunger = hunger + 20;
+
+            return true;
+
+        }
+            return false;
+
+    }
+}
+
+
+
--- src/main/java/virtual_pet/VirtualPetApplication.java
+++ src/main/java/virtual_pet/VirtualPetApplication.java
@@ -1,9 +1,60 @@
 package virtual_pet;
 
+import java.util.Scanner;
+
 public class VirtualPetApplication {
 
     public static void main(String[] args) {
         //Interact with a VirtualPet object in this method
+        Scanner input = new Scanner(System.in);
+        VirtualPet pet = new VirtualPet();
+        boolean end = false;
+
+        int answer;
+
+        while (!end) {
+
+           if (pet.random()){
+               continue;
+           }
+            System.out.println("Andy the Ant-Eater");
+            System.out.println("Hunger: " + pet.hunger);
+            System.out.println("Thirst: " + pet.thirst);
+            System.out.println("Boredom: " + pet.boredom);
+            System.out.println("Tiredness: " + pet.tiredness);
+
+            answer = input.nextInt();
+
+            if (answer == 1) {
+                pet.feed();
+                System.out.println("You feed Andy!");
+
+            } else if (answer == 2) {
+                pet.drink();
+                System.out.println("You give Andy water!");
+            } else if (answer == 3) {
+                pet.play();
+                System.out.println("You play with Andy!");
+            } else if (answer == 4) {
+                pet.sleep();
+                System.out.println("You let Andy sleep!");
+            } else if (answer == 5) {
+                System.out.println("Thank you for playing!");
+                end = true;
+            }
+
+            pet.tick();
+
+
+        }
+
+        //System.out.println("What is your pets name?");
+        //  pet.name = input.nextLine();
+
+        // System.out.println(pet.name);
+
+
     }
+
 
 }
