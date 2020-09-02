b6ee0d2710c69d9f56c870d486231c5697d7fcaa
robertson355 <barry.robertson1993@gmail.com>
2020-09-02 13:58:08 -0400

    Updated Thread to work with static variables incrementing

--- src/main/java/virtual_pet/VirtualPet.java
+++ src/main/java/virtual_pet/VirtualPet.java
@@ -2,58 +2,61 @@ package virtual_pet;
 
 public class VirtualPet {
 
-   int  hunger = 40;
-   int thirst = 100;
-   int boredom = 40;
-   int tiredness = 120;
-
-   public void tick() {
-
-    hunger = hunger - 4;
-    thirst =  thirst - 2;
+    public static int ticker = 0;
+    public static int hunger = 100;
+    public static int thirst = 100;
+    public static int boredom = 100;
+    public static int tiredness = 100;
+    public static int random = 0;
+    public static int shutdownButton = 0;
 
 
-   }
+    public int change(int number) {
+
+
+        ticker = ticker + number;
+        return ticker;
+
+    }
 
     public void feed() {
 
+        hunger = hunger + 40;
+        thirst = thirst - 5;
 
-       hunger = hunger + 20;
-       thirst = thirst - 5;
+
 
     }
 
     public void drink() {
-       hunger = hunger - 5;
-       thirst = thirst + 15;
+        hunger = hunger - 5;
+        thirst = thirst + 15;
     }
 
     public void play() {
-       hunger = hunger -5;
-       thirst = thirst -10;
-       boredom = boredom + 30;
+        hunger = hunger -5;
+        thirst = thirst -10;
+        boredom = boredom + 30;
     }
 
     public void sleep() {
-       hunger = hunger -10;
-       thirst = thirst -10;
-       tiredness = tiredness + 30;
+        hunger = hunger -10;
+        thirst = thirst -10;
+        tiredness = tiredness + 30;
     }
 
-
-
-    public  random() {
+    public void random() {
         int random = (int) (1 + (Math.random() * 5));
 
         if (random == 2) {
             System.out.println("Your pet decided to eat!");
-            hunger = hunger + 20;
-
-            return true;
-
+            feed();
         }
-            return false;
 
+    }
+
+    public boolean shutdown() {
+        return shutdownButton == 9;
     }
 }
 
--- src/main/java/virtual_pet/VirtualPetApplication.java
+++ src/main/java/virtual_pet/VirtualPetApplication.java
@@ -2,74 +2,23 @@ package virtual_pet;
 
 import java.util.Scanner;
 
-public class VirtualPetApplication {
-
+public class VirtualPetApplication extends Thread {
     public static void main(String[] args) throws InterruptedException {
-        //Interact with a VirtualPet object in this method
-       // Scanner input = new Scanner(System.in);
-        VirtualPet pet = new VirtualPet();
+        Scanner input = new Scanner(System.in);
+        VirtualPetApplication thread = new VirtualPetApplication();
+        VirtualPet ct = new VirtualPet();
+        thread.start();
         boolean end = false;
-        int time = 0;
-        String input = null;
 
-        int answer;
 
         while (!end) {
-            input = inputReader.next();
-
-            System.out.println(time);
-            Thread.sleep(2000);
-
-            time++;
-        }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-           /* if (pet.random()) {
-                continue;
-            }
             System.out.println("Andy the Ant-Eater");
-            System.out.println("Hunger: " + pet.hunger);
-            System.out.println("Thirst: " + pet.thirst);
-            System.out.println("Boredom: " + pet.boredom);
-            System.out.println("Tiredness: " + pet.tiredness);
+            System.out.println("Hunger: " + VirtualPet.hunger);
+            System.out.println("Thirst: " + VirtualPet.thirst);
+            System.out.println("Boredom: " + VirtualPet.boredom);
+            System.out.println("Tiredness: " + VirtualPet.tiredness);
             System.out.println();
             System.out.println("Press the corresponding number to do an action!");
             System.out.println("1: Feed Andy");
@@ -78,39 +27,80 @@ public class VirtualPetApplication {
             System.out.println("4: Put Andy in bed");
             System.out.println("5: Do nothing.");
             System.out.println("9: Quit the program");
-            answer = input.nextInt();
+
+            int answer = input.nextInt();
 
             if (answer == 1) {
-                pet.feed();
+                ct.feed();
                 System.out.println("You feed Andy!");
 
             } else if (answer == 2) {
-                pet.drink();
+                ct.drink();
                 System.out.println("You give Andy water!");
             } else if (answer == 3) {
-                pet.play();
+                ct.play();
                 System.out.println("You play with Andy!");
             } else if (answer == 4) {
-                pet.sleep();
+                ct.sleep();
                 System.out.println("You let Andy sleep!");
             } else if (answer == 9) {
+                VirtualPet.shutdownButton = 9;
                 System.out.println("Thank you for playing!");
                 end = true;
             }
 
-            Thread.sleep(2000);
-            pet.tick();*/
-
 
         }
 
-        //System.out.println("What is your pets name?");
-        //  pet.name = input.nextLine();
-
-        // System.out.println(pet.name);
 
 
     }
+    public void run() {
+        VirtualPet ct = new VirtualPet();
+        boolean finished = false;
+
+        while (!finished){
+
+            if (ct.shutdown()) {
+                finished = true;
+            }
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            VirtualPet.ticker++;
+            VirtualPet.hunger--;
+            // System.out.println(ClassTest.hunger);
+            VirtualPet.thirst--;
+            VirtualPet.boredom--;
+            VirtualPet.tiredness--;
+            VirtualPet.random++;
+            ct.random();
+
+
+            //int random = (int) (1 + (Math.random() * 5));
+
+
+
+
+
+
+
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
 
 
 
